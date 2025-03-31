d4c6e2dfbbbe87fd42fede76df095d2b39da9eba
ericmed1 <eric_ivan1@yahoo.com>
2025-03-31 15:54:21 -0500

    more updates

--- rnn.py
+++ rnn.py
@@ -31,13 +31,14 @@ class RNN(nn.Module):
 
     def forward(self, inputs):
         # [to fill] obtain hidden layer representation (https://pytorch.org/docs/stable/generated/torch.nn.RNN.html)
-        _, hidden = 
+        output, _ = self.rnn(inputs)
         # [to fill] obtain output layer representations
-
+        summed_output = torch.sum(output, dim=0)
         # [to fill] sum over output 
-
+        output = self.W(summed_output)
         # [to fill] obtain probability dist.
-
+        predicted_vector = self.softmax(output)
+        
         return predicted_vector
 
 
